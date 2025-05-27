eb38c7ce00bdb7cb151e18dc03c5eee6c12c879b
danielkostrzewski-bib <daniel.kostrzewski@accenture.com>
2025-05-27 15:32:30 +0200

    Update index.js

--- hobby-horse-app/index.js
+++ hobby-horse-app/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 const DATA_FILE = 'data.txt';
 
 app.use(express.urlencoded({ extended: true }));
